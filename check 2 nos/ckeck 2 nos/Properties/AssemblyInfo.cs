fecbefb0447414783bd7d857e21172196598ee75
KKaviya1 <Kaviyagandhi136@gmail.com>
2023-08-03 16:12:37 +0530

    my commit

---
+++ check 2 nos/ckeck 2 nos/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ckeck 2 nos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ckeck 2 nos")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa0ed62a-4286-4b7a-a255-872a2842de7c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ check 2 nos/ckeck 2 nos/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using checkTwoNumbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace checkTwoNumbers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int num1 = 5;
+            int num2 = 5;
+
+            bool result = NumberComparer.AreNumbersEqual(num1, num2);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void AreNumbersEqual_NumbersNotEqual_ReturnsFalse()
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            bool result = NumberComparer.AreNumbersEqual(num1, num2);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
+
+
+       
